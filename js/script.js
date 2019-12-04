e5a34ec91a45087f1012e83f1e21def2c201ac26
Ilya <h82st8@gmail.com>
2019-12-04 18:47:51 +0300

    seventh

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-	document.querySelector('.choice_wrap').addEventListener('click', func);
+document.querySelector('.choice_wrap').addEventListener('click', func);
 
-	function func(event) {
-		console.log(event);
+function func(event) {
+	console.log(event);
 
-	// let textInput = document.getElementById("btn1");
-	// let btnName = textInput.value;
+	let btn = document.getElementsByClassName('choice__item');
 
 	if (event.target.classList[0] == 'choice__item') {
-		let btn = document.getElementsByClassName('choice__item');
 
 		for(let i = 0; i < btn.length; i++){
 			btn[i].classList.remove('btn_active');
-			// let li = document.createElement("li");
-			// li.innerHTML = btnName;
-			// let ul = document.getElementById("playlist");
-			// ul.appendChild(li);
 		}
 		event.target.classList.add('btn_active');
+
 	}
-}
+
+	let tagged = document.querySelector('.btn_active');
+	let taggedName = tagged.value;
+
+	let li = document.querySelector("#li");
+	li.innerHTML = taggedName;
+
+	}
