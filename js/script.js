c56e429433c3a385b05ba7c9280c5e2b5041047a
Ilya <h82st8@gmail.com>
2019-12-04 13:09:44 +0300

    sixth

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-document.querySelector('.choice_wrap').addEventListener('click', func);
+	document.querySelector('.choice_wrap').addEventListener('click', func);
 
-function func(event) {
-	console.log(event);
+	function func(event) {
+		console.log(event);
+
+	// let textInput = document.getElementById("btn1");
+	// let btnName = textInput.value;
+
 	if (event.target.classList[0] == 'choice__item') {
 		let btn = document.getElementsByClassName('choice__item');
 
 		for(let i = 0; i < btn.length; i++){
 			btn[i].classList.remove('btn_active');
+			// let li = document.createElement("li");
+			// li.innerHTML = btnName;
+			// let ul = document.getElementById("playlist");
+			// ul.appendChild(li);
 		}
 		event.target.classList.add('btn_active');
 	}
