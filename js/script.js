42de3e8707058c7fc79c14c7f177f5c2e7909a32
Ilya <h82st8@gmail.com>
2019-12-05 11:01:40 +0300

    9

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-document.querySelector('.choice_wrap').addEventListener('click', func);
+document.getElementById('btn11').addEventListener('click', fun1);
+document.getElementById('btn22').addEventListener('click', fun2);
+document.getElementById('btn33').addEventListener('click', fun3);
+document.getElementById('btn44').addEventListener('click', fun4);
+
+
+function fun1() {
+	document.getElementById('cont1').style.display='none';
+	document.getElementById('cont2').style.display='block';
+}
+
+function fun2() {
+	document.getElementById('cont2').style.display='none';
+	document.getElementById('cont3').style.display='block';
+}
+
+function fun3() {
+	document.getElementById('cont3').style.display='none';
+	document.getElementById('cont4').style.display='block';
+}
+
+function fun4() {
+	document.getElementById('cont4').style.display='none';
+	document.getElementById('cont5').style.display='block';
+}
+
+	document.querySelector('.choice_wrap').addEventListener('click', func);
+	document.querySelector('.choice_wrap_2').addEventListener('click', func2);
+	document.querySelector('.choice_wrap_3').addEventListener('click', func3);
+	document.querySelector('.choice_wrap_4').addEventListener('click', func4);
+// loadPlaylist();
 
 function func(event) {
 	console.log(event);
@@ -19,5 +49,86 @@ function func(event) {
 
 	let li = document.querySelector("#li");
 	li.innerHTML = taggedName;
+	let input = document.querySelector("#input1");
+	input.value = taggedName;
+	// save(taggedName);
+
+}
+
+function func2(event) {
+	console.log(event);
+
+	let btn = document.getElementsByClassName('choice__item');
+
+	if (event.target.classList[0] == 'choice__item') {
+
+		for(let i = 0; i < btn.length; i++){
+			btn[i].classList.remove('btn_active');
+		}
+		event.target.classList.add('btn_active');
 
 	}
+
+	let tagged = document.querySelector('.btn_active');
+	let taggedName = tagged.value;
+
+	let li = document.querySelector("#li2");
+	li.innerHTML = taggedName;
+	let input = document.querySelector("#input2");
+	input.value = taggedName;
+
+	// save(taggedName);
+
+}
+
+function func3(event) {
+	console.log(event);
+
+	let btn = document.getElementsByClassName('choice__item');
+
+	if (event.target.classList[0] == 'choice__item') {
+
+		for(let i = 0; i < btn.length; i++){
+			btn[i].classList.remove('btn_active');
+		}
+		event.target.classList.add('btn_active');
+
+	}
+
+	let tagged = document.querySelector('.btn_active');
+	let taggedName = tagged.value;
+
+	let li = document.querySelector("#li3");
+	li.innerHTML = taggedName;
+	let input = document.querySelector("#input3");
+	input.value = taggedName;
+
+	// save(taggedName);
+
+}
+
+function func4(event) {
+	console.log(event);
+
+	let btn = document.getElementsByClassName('choice__item');
+
+	if (event.target.classList[0] == 'choice__item') {
+
+		for(let i = 0; i < btn.length; i++){
+			btn[i].classList.remove('btn_active');
+		}
+		event.target.classList.add('btn_active');
+
+	}
+
+	let tagged = document.querySelector('.btn_active');
+	let taggedName = tagged.value;
+
+	let li = document.querySelector("#li4");
+	li.innerHTML = taggedName;
+	let input = document.querySelector("#input4");
+	input.value = taggedName;
+
+	// save(taggedName);
+
+}
---
+++ mail.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'libs/phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$style = $_POST['style'];
+$color = $_POST['color'];
+$material = $_POST['material'];
+$money = $_POST['money'];
+$phone = $_POST['phone'];
+$Email = $_POST['mail'];
+$text = $_POST['text'];
+$theme = $_POST['theme'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'as@reallife.pro'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'qweasdzxc777zxc'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('as@reallife.pro'); // от кого будет уходить письмо?
+$mail->addAddress('i.shurmanov@reallife.pro');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = 'Пользователь:  ' . $name . '  ' . $phone . '  ' . $Email . '  оставил заявку <br>' . $style . '<br>' . $color . '<br>' . $material . '<br>' . $money;
+$mail->AltBody = '';
+
+if($mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
